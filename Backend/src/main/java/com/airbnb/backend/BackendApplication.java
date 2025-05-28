eeddaae0cb080f4a6b2d4de024a70ce9893d6176
Frederik <fred@hess-pedersen.dk>
2025-05-28 16:46:29 +0200

    updated application.yml to work with docker files

--- Backend/src/main/java/com/airbnb/backend/BackendApplication.java
+++ Backend/src/main/java/com/airbnb/backend/BackendApplication.java
@@ -3,9 +3,7 @@ package com.airbnb.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class  // “Chill, I don’t have a database yet. Don’t try to set one up.”
-})
+@SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
